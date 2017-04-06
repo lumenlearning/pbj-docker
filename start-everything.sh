0cf5754e7fb41a5f189842278d8b9246c8b6a98d
Michael Ackerman <michael@lumenlearning.com>
2017-04-06 16:05:28 -0700

    Attempt to stop local Apache before starting containers.

--- start-everything.sh
+++ start-everything.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+set -euo pipefail
+
+# Ensure local Apache is stopped
+PROCS=`ps auxw | grep -E '[h]ttpd|[a]pache' | wc -l`
+
+if [ "$PROCS" != "0" ]; then
+  echo -e "\n*** Enter your password to sudo and stop local Apache (if prompted)...\n"
+  sudo apachectl stop
+fi
+
+# Start PBJ containers
 mkdir -p ~/pbj
 mkdir -p ~/pbj-db
 
 docker-compose up
-
